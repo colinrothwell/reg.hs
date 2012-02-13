fe3bae8ae2b46b2236d2e81a3cb8b445afe9f932
Colin Rothwell <cr437@cam.ac.uk>
2012-02-13 11:16:46 +0000

    One function to execute instructions

--- reg.hs
+++ reg.hs
@@ -11,24 +11,24 @@ type Program = Array.Array Integer Instruction
 type Store = Map.Map Register Integer 
 type ProgramCounter = Integer
 
-add :: Store -> Register -> Store
-add s r = Map.insert r (Map.findWithDefault 0 r s + 1) s
+data State = Continue Store ProgramCounter
+           | Stop
 
-sub :: Store -> Register -> Integer -> Integer -> (Store, Integer)
-sub s r pcnb pcb
-    | (s Map.! r) == 0 = (s, pcb)
-    | otherwise = (Map.insert r (Map.findWithDefault 0 r s - 1) s, pcnb)
+execute :: Store -> Instruction -> State
+execute _ Halt = Stop
+execute s (Add r pc) = Continue (Map.insert r (Map.findWithDefault 0 r s + 1) s) pc
+execute s (Sub r pcnb pcb)
+    | (s Map.! r) == 0 = Continue s pcb
+    | otherwise = Continue (Map.insert r (Map.findWithDefault 0 r s - 1) s) pcnb
 
-run :: Program -> Store -> ProgramCounter -> Integer
+run :: Program -> Store -> ProgramCounter -> Store
 run prog store pc =
-    case ins of Halt -> store Map.! "R0"
-                (Add r pc') -> run prog (add store r) pc'
-                (Sub r pcnb pcb) -> 
-                    let (s', pc') = sub store r pcnb pcb
-                    in run prog s' pc'
+    case ns of Stop -> store
+               (Continue s' pc') -> run prog s' pc'
     where ins = prog Array.! pc
+          ns = execute store ins
 
 main = 
     let pl = [Sub "R1" 1 2, Add "R0" 0, Sub "R2" 3 4, Add "R0" 2, Halt]
         s = Map.fromList [("R0", 0), ("R1", 5), ("R2", 10)]
-    in print $ run (Array.listArray (0, 4) pl) s 0
+    in print $ run (Array.listArray (0, 4) pl) s 0 Map.! "R0"
