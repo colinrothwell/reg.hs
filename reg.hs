39d06a04fddfe86682855171e28eba59b94ca642
Colin Rothwell <cr437@cam.ac.uk>
2012-02-15 00:27:04 +0000

    Can now read register machines in from files.

--- reg.hs
+++ reg.hs
@@ -1,19 +1,42 @@
 import qualified Data.Map as Map
 import qualified Data.Array as Array
+import Data.Char (toLower)
+import System (getArgs)
 
 type Register = String
+type ProgramCounter = Int
 
 data Instruction = Halt 
-                 | Add Register Integer
-                 | Sub Register Integer Integer
+                 | Add Register ProgramCounter
+                 | Sub Register ProgramCounter ProgramCounter
+                 deriving (Show)
 
-type Program = Array.Array Integer Instruction
-type Store = Map.Map Register Integer 
-type ProgramCounter = Integer
+type Program = Array.Array ProgramCounter Instruction
+type Store = Map.Map Register Integer
 
 data State = Continue Store ProgramCounter
            | Stop
 
+readBranch :: [String] -> Int -> ProgramCounter
+readBranch ss i = read $ ss !! i
+
+parseInstruction :: String -> Instruction
+parseInstruction s 
+    | op == '+' = Add reg (b 1)
+    | op == '-' = Sub reg (b 1) (b 2)
+    | lf == "halt" = Halt
+    | otherwise = error $ "Invalid Instruction Format " ++ s
+    where parts = words s
+          f = head parts
+          lf = map toLower f
+          b = readBranch parts
+          reg = init f
+          op = last f
+
+parseProgram :: String -> Program
+parseProgram s = Array.listArray (0, length il - 1) il
+    where il = map parseInstruction (lines s)
+        
 execute :: Store -> Instruction -> State
 execute _ Halt = Stop
 execute s (Add r pc) = Continue (Map.insert r (Map.findWithDefault 0 r s + 1) s) pc
@@ -29,6 +52,8 @@ run prog store pc =
           ns = execute store ins
 
 main = 
-    let pl = [Sub "R1" 1 2, Add "R0" 0, Sub "R2" 3 4, Add "R0" 2, Halt]
-        s = Map.fromList [("R0", 0), ("R1", 5), ("R2", 10)]
-    in print $ run (Array.listArray (0, 4) pl) s 0 Map.! "R0"
+    let s = Map.fromList [("R0", 0), ("R1", 5), ("R2", 10)]
+    in getArgs >>= 
+       return . head >>=
+       readFile >>= 
+       \x -> print $ run (parseProgram x) s 0 Map.! "R0"
