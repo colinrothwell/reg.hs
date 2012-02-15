495b63b263a4d7380f433af806be60974a69e6a8
Colin Rothwell <cr437@cam.ac.uk>
2012-02-15 13:59:20 +0000

    Removed old extractLabels

--- reg.hs
+++ reg.hs
@@ -20,26 +20,6 @@ data State = Continue Store ProgramCounter
 
 type LabelMap = Map.Map String ProgramCounter
 
-{-extractLabels :: String -> (LabelMap, String)-}
-{-extractLabels p = (lm, unlines pl)-}
-    {-where ls = lines p-}
-          {-extractLabel :: LabelMap -> ProgramCounter -> String -> (LabelMap, String)-}
-          {-extractLabel lm pc isn-}
-              {-| islbl = (Map.insert lbl pc lm, isn')-}
-              {-| otherwise = (lm, isn)-}
-              {-where wi = words isn-}
-                    {-cl = head wi-}
-                    {-islbl = (last cl == ':')-}
-                    {-lbl = init cl-}
-                    {-isn' = unwords $ tail wi-}
-          {-transform :: LabelMap -> ProgramCounter -> [String] -> (LabelMap, [String])-}
-          {-transform lm pc (i:is) = (lm'', il') -}
-              {-where (lm', i') = extractLabel lm pc i-}
-                    {-(lm'', is') = transform lm' (pc + 1) is-}
-                    {-il' = i' : is'-}
-          {-transform lm _ [] = (lm, [])-}
-          {-(lm, pl) = transform Map.empty 0 ls-}
-
 extractLabels :: String -> (LabelMap, String)
 extractLabels p = (rdlbls pl Map.empty 0, unlines $ rmlbls pl)
     where pl = lines p
